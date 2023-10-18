36a82f323fc56fed3a7424dfd423effedd560f4f
MaryamQandeel <123130072+MaryamQandeel@users.noreply.github.com>
2023-10-19 00:55:16 +0300

    Create usrimmed_signalhandle.c

---
+++ usrimmed_signalhandle.c
@@ -0,0 +1,29 @@
+#include "shell.h"
+
+/**
+* usrimmed - extract $ to tell user what program is....
+*
+* Stamps the prompt if the shell is in interactive mode
+* ON initializing to take their input
+*
+* Return: no return
+*/
+void usrimmed(void)
+{
+	if ((isatty(STDIN_FILENO) == 1) && (isatty(STDOUT_FILENO) == 1))
+		flags.interactive = 1;
+	if (flags.interactive)
+		write(STDERR_FILENO, "$ ", 2);
+}
+/**
+ *signalhandler- continue to maintain the track is interactive mode
+ *@m: the signal number
+ *Return: nothing
+ */
+
+void signalhandle(int m)
+{
+	(void)m;
+	write(STDERR_FILENO, "\n", 1);
+	write(STDERR_FILENO, "$ ", 2);
+}
