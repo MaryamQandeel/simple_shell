9156e2a099f33a6788381ef0ab029d9a9cd738a1
MaryamQandeel <123130072+MaryamQandeel@users.noreply.github.com>
2023-10-19 01:13:08 +0300

    Update path -env.c

--- path -env.c
+++ path -env.c
@@ -43,30 +43,6 @@ char *pathy(char *path, char *command)
 }
 
 /**
- * pathtesting - checks whether path is valid
- *
- * @command: The user-made or entered command
- * @path: symbolized
- *
- * Return: directed in order to succeed
- */
-char *pathtesting(char **path, char *command)
-{
-    int i = 0;
-    char *output;
-
-    while (path[i])
-    {
-        output = pathy(path[i], command);
-        if (access(output, F_OK | X_OK) == 0)
-            return (output);
-        free(output);
-        i++;
-    }
-    return (NULL);
-}
-
-/**
  * findlocatepath - finds path from global environment
  *
  * Return: NULL if the path is not detected, or the path if it is detected
