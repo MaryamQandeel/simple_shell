f16e5416a2e2bac938958237ce4e8c191ca60d78
MaryamQandeel <123130072+MaryamQandeel@users.noreply.github.com>
2023-10-19 00:45:37 +0300

    Update path -env.c

--- path -env.c
+++ path -env.c
@@ -1,13 +1,13 @@
 #include "shell.h"
 
 /**
-* pathend - path append it adds the path to the command directly
+* pathy - path append it adds the path to the command directly
 *
 * @command: the user entered the command
 * @path: path command
 * Return: the buffer that includes is on my track order on success
 */
-char *pathend(char *path, char *command)
+char *pathy(char *path, char *command)
 {
 	char *buff;
 	size_t i = 0, j = 0;
@@ -40,6 +40,30 @@ char *pathend(char *path, char *command)
 	}
 	buff[i + j] = '\0';
 	return (buff);
+}
+
+/**
+* pathtesting - checks whether path is valid
+*
+* @command: The user made or entered the command
+* @path: symbolzed
+*
+* Return: directed in order to succeed
+*/
+char *pathtesting(char **path, char *command)
+{
+	int i = 0;
+	char *output;
+
+	while (path[i])
+	{
+		output = pathy(path[i], command);
+		if (access(output, F_OK | X_OK) == 0)
+			return (output);
+		free(output);
+		i++;
+	}
+	return (NULL);
 }
 
 /**
