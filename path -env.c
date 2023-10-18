cf3cdb76a8b0b67a673e81b014b342a22f7fb41e
MaryamQandeel <123130072+MaryamQandeel@users.noreply.github.com>
2023-10-19 01:22:19 +0300

    env.c

--- path -env.c
+++ path -env.c
@@ -1,48 +1,6 @@
 #include "shell.h"
 
 /**
- * pathy - path append; it adds the path to the command directly
- *
- * @command: the user-entered command
- * @path: path command
- * Return: the buffer that includes it on my track order on success
- */
-char *pathy(char *path, char *command)
-{
-    char *buff;
-    size_t i = 0, j = 0;
-
-    if (command == 0)
-        command = "";
-
-    if (path == 0)
-        path = "";
-
-    buff = malloc(sizeof(char) * (stringlen(path) + stringlen(command) + 2));
-    if (!buff)
-        return (NULL);
-
-    while (path[i])
-    {
-        buff[i] = path[i];
-        i++;
-    }
-
-    if (path[i - 1] != '/')
-    {
-        buff[i] = '/';
-        i++;
-    }
-    while (command[j])
-    {
-        buff[i + j] = command[j];
-        j++;
-    }
-    buff[i + j] = '\0';
-    return (buff);
-}
-
-/**
  * envprnt - prints environment mix to stdout
  *
  * Return: nothing 0
