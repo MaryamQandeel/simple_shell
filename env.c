b49f80556edb2e717a301f8b32be3136bdf45649
maryamQndeel <maryam.qandeel@yahoo.com>
2023-10-18 15:34:22 -0700

    t

--- env.c
+++ env.c
@@ -7,13 +7,13 @@
  */
 void envprnt(void)
 {
-    int x = 0;
-    char **envy = environ;
+	int x = 0;
+	char **envy = environ;
 
-    while (envy[x])
-    {
-        write(STDOUT_FILENO, (const void *)envy[x], stringlen(envy[x]));
-        write(STDOUT_FILENO, "\n", 1);
-        x++;
-    }
+	while (envy[x])
+	{
+		write(STDOUT_FILENO, (const void *)envy[x], stringlen(envy[x]));
+		write(STDOUT_FILENO, "\n", 1);
+		x++;
+	}
 }
